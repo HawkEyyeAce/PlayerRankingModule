efc2ccc15bf4f8627761161848afe55d1ddcc5fe
HawkEyyeAce <elodie.grosmaire@hotmail.fr>
2020-09-09 19:42:15 +0200

    Draft of Page2 and connection between pages

---
+++ Assets/Scripts/SwitchMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchMenu : MonoBehaviour
+{
+    public void ShowRanks()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackToMainPage()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void SwitchPage(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
